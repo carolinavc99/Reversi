d849c42c04b4d400994d73bbb430fffe1fb27f08
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2019-03-31 21:32:00 +0100

    fhbdsjnkd

---
+++ proj/interpretador.h
@@ -0,0 +1,9 @@
+#ifndef REVERSI_INTERPRETADOR_H
+#define REVERSI_INTERPRETADOR_H
+#include "estado.h"
+
+#define MAX_BUFFER 1024
+
+void interpretador (ESTADO e);
+
+#endif //REVERSI_INTERPRETADOR_H
