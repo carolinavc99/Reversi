c23edf852cc4708ff04a6fe3fb9a1184c839cb4e
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2019-04-01 14:54:29 +0100

    limpeza

--- proj/interpretador.h
+++
@@ -1,9 +0,0 @@
-#ifndef REVERSI_INTERPRETADOR_H
-#define REVERSI_INTERPRETADOR_H
-#include "estado.h"
-
-#define MAX_BUFFER 1024
-
-void interpretador (ESTADO e);
-
-#endif //REVERSI_INTERPRETADOR_H
