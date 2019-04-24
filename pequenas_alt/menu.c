f786730e6dba00e1528fd64be26b66e7258fa8aa
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2019-04-24 09:45:20 +0100

    peq alt

---
+++ pequenas_alt/menu.c
@@ -0,0 +1,147 @@
+#include "menu.h"
+#include "comandos.h"
+#include "stack.h"
+#include <stdio.h>
+#include <ctype.h>
+
+ESTADO menu (ESTADO e) { // interpretador
+    char opcao[1024], arg1='\0', arg2='\0';
+    char input[1024];
+    ESTADO v;
+    int numero=0; // numero de (opcao + argumentos) inseridos
+
+    topo = NULL;
+
+    // número de jogadas efetuadas no ponto atual do jogo
+    int jogadas_efetuadas = 0;
+    // novajogada incrementa este valor
+
+
+    while (toupper(opcao[0]) != 'Q') {
+
+        printf("\n. . . . . . . . . . . . . . . . . . . . . . . . .\n"
+                 ". Selecione a opção:                            .\n"
+                 ".                                               .\n"
+                 ". (N) Novo jogo                                 .\n"
+                 ". (L) Continuar jogo                            .\n"
+                 ". (E) Guardar jogo                              .\n"
+                 ". (J) Jogar                                     .\n"
+                 ". (S) Jogadas válidas                           .\n"
+                 ". (H) Sugestão de jogada                        .\n"
+                 ". (U) Desfazer última jogada                    .\n"
+                 ". (A) Novo jogo contra o bot                    .\n"
+                 ". (Q) Sair                                      .\n"
+                 ". . . . . . . . . . . . . . . . . . . . . . . . .\n");
+
+
+        fgets(input, 1000, stdin); // guarda em 'input', well, o input
+
+        numero = sscanf(input, "%s", opcao);
+
+        opcao[0] = toupper(opcao[0]);
+
+        // prints-teste -- eliminar
+        printf("input <%s>\n", input);
+        printf("opcao <%s>\n", opcao);
+
+
+        switch (opcao[0]) {
+            case 'N':
+                sscanf(input, "%*s %c", &arg1);
+                arg1 = toupper(arg1);
+
+                if (chrtovalor(arg1) == VALOR_X || chrtovalor(arg1) == VALOR_O) {
+                    e = novoJogo(chrtovalor(arg1));
+                    resetJogadas(e);
+                    push(e);
+                }
+                else
+                    printf("Peça inválida\n");
+
+                break;
+
+            case 'L':                                   //  <-- acaba quando se dá um nome que não existe
+                sscanf(input, "%*s %s", &arg1);
+                e = continuaJogo (e, &arg1);
+                jogadas_efetuadas = 0;
+                break;
+
+            case 'E':
+                sscanf(input, "%*s %s", &arg1);
+                if (e.peca == VAZIA)
+                    printf("\nCrie um novo jogo ou continue um jogo já existente.\n");
+                //else if (!arg1) {
+                //    printf("\nPrecisa de indicar um nome para guardar o ficheiro.\n");
+                //}
+                else {
+                    guardar(e, &arg1);
+                    resetJogadas(e);
+                    push(e);
+                    jogadas_efetuadas = 0;
+                }
+                break;
+
+            case 'J':
+                sscanf(input, "%*s %c %c", &arg1, &arg2);
+                if (!arg2)
+                    printf("\nFalta de argumentos.\n");
+                else if (validarJogada(e, arg1, arg2) == 1)
+                        printf("\nJogada inválida.\n");
+                else if (e.peca == VAZIA)
+                            printf("\nCrie um novo jogo ou continue um jogo já existente.\n");
+                else {
+                    e = jogar(e, arg1, arg2);  // coloca a peça no sítio, se tal for possível
+                    jogadas_efetuadas = novaJogada(e, arg1, arg2, jogadas_efetuadas);
+                }
+                 break;
+
+            case 'A':
+                sscanf(input, "%*s %c", &arg1);
+                //bot(arg1);    // a função bot funciona dependendo da dificuldade do jogo
+                 break;
+
+            case 'S':
+             if(e.peca == VAZIA)
+                 printf("\nCrie um novo jogo ou continue um jogo já existente.\n");
+             else {
+                 v = calcJogadasValidas(e);
+                 posicoesValidas(e, v);
+             }
+                 break;
+
+             case 'H':
+                 if(e.peca == VAZIA)
+                     printf("\nCrie um novo jogo ou continue um jogo já existente.\n");
+                 // else
+                 break;
+
+             case 'U':
+                 e = score(e);
+                 if (jogadas_efetuadas == 0) {
+                      printf("Não há mais jogadas para retroceder.");
+                 }
+                 else if(e.peca == VAZIA)
+                     printf("\nCrie um novo jogo ou continue um jogo já existente.\n");
+                 else {
+                     e = undo(e);
+                     jogadas_efetuadas--;
+                 }
+                 break;
+
+             case 'Q':
+                 printf("\nSaiu do jogo.\n"
+                        "\n"
+                        "\nTrabalho realizado por:"
+                        "\n Ana Sofia Gomes; Carolina Vila Chã; Miguel Rodrigues");
+                 break;
+
+             default:
+                 printf("\nOpção inválida.\n");
+                 break;
+            }
+        }
+
+    //  NOTA:
+    //  uma vez guardado um jogo, a lista de jogadas prévias desaparece
+    return e;
+}
