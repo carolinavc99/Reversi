fb937bd6d0792ab04fcb114a8c94c50037fca0c1
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2019-04-26 13:30:08 +0100

    tirei o txt e mexi em basicamente tudo

---
+++ SemTxt/bot.c
@@ -0,0 +1,5 @@
+//
+// Created by carolinavc on 4/21/19.
+//
+
+#include "bot.h"
---
+++ SemTxt/bot.h
@@ -0,0 +1,11 @@
+//
+// Created by carolinavc on 4/21/19.
+//
+
+#ifndef PROJ_BOT_H
+#define PROJ_BOT_H
+
+
+
+
+#endif //PROJ_BOT_H
