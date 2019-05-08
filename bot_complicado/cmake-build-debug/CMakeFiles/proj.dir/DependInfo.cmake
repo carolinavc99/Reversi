1f07e5052bb750abae9b5ac6e2df073129061a7a
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2019-05-08 16:26:46 +0100

    bot andado

---
+++ bot_complicado/cmake-build-debug/CMakeFiles/proj.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/media/sf_LI2/bot_start/bot.c" "/media/sf_LI2/bot_start/cmake-build-debug/CMakeFiles/proj.dir/bot.c.o"
+  "/media/sf_LI2/bot_start/comandos.c" "/media/sf_LI2/bot_start/cmake-build-debug/CMakeFiles/proj.dir/comandos.c.o"
+  "/media/sf_LI2/bot_start/estado.c" "/media/sf_LI2/bot_start/cmake-build-debug/CMakeFiles/proj.dir/estado.c.o"
+  "/media/sf_LI2/bot_start/main.c" "/media/sf_LI2/bot_start/cmake-build-debug/CMakeFiles/proj.dir/main.c.o"
+  "/media/sf_LI2/bot_start/menu.c" "/media/sf_LI2/bot_start/cmake-build-debug/CMakeFiles/proj.dir/menu.c.o"
+  "/media/sf_LI2/bot_start/stack.c" "/media/sf_LI2/bot_start/cmake-build-debug/CMakeFiles/proj.dir/stack.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
