f6a311294d79c35501fec92fbc142f23b44af931
AnnieSophie <48517703+AnnieSophie@users.noreply.github.com>
2019-05-10 17:18:51 +0100

    Falta undo do Bot e opção passar jogada.

---
+++ Bot Level 1 Done/estado.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#ifndef PROJ_ESTADO_H
+#define PROJ_ESTADO_H
+
+
+
+
+/**
+estado.h
+Definição do estado i.e. tabuleiro. Representação matricial do tabuleiro.
+*/
+
+
+// definição de valores possiveis no tabuleiro
+typedef enum {VAZIA, VALOR_X, VALOR_O, VALOR_V, VALOR_S} VALOR;
+
+/**
+Estrutura que armazena o estado do jogo
+*/
+typedef struct estado {
+    VALOR peca;          // peça do jogador que vai jogar!
+    VALOR grelha[8][8];
+    char modo;           // modo em que se está a jogar! 0-> manual,
+                                                      // 1-> contra computador nível 1
+                                                      // 2-> contra computador nível 2
+                                                      // 3-> contra computador nível 3
+    VALOR bot;            // peça do Bot
+    char nome[30];       // nome do ficheiro de jogo (INCLUI '.txt') -- é '.' se ainda não tiver sido guardado
+    int score[2];
+} ESTADO;
+
+// >>>> ACRESCENTEI STRUCT <<<<
+/**
+ * Estrutura que armazenas as coordenadas de um ponto
+ */
+typedef struct ponto {
+    int l; //linha
+    int c; //coluna
+} PONTO;
+
+
+void printa(ESTADO);
+
+ESTADO score (ESTADO e);
+
+VALOR chrtovalor(char c);
+
+int min(int a, int b);
+int max(int a, int b);
+
+#endif //PROJ_ESTADO_H
+
