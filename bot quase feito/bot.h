84d2ff30ba7fca3bec4a1d78b8741c7c46ff90ec
carolinavc99 <48517052+carolinavc99@users.noreply.github.com>
2019-05-18 22:13:33 +0100

    bot ainda bugado

---
+++ bot quase feito/bot.h
@@ -0,0 +1,57 @@
+//
+// Created by carolinavc on 4/21/19.
+//
+
+#ifndef PROJ_BOT_H
+#define PROJ_BOT_H
+
+#include "stack.h" // para a definição de JOGADA
+
+ESTADO bot (ESTADO e);
+
+typedef struct famJog {
+    ESTADO estado;
+    int nivel;
+    // jogada que ele contém
+    int linha;
+    int coluna;
+
+    struct famJog *one;
+    struct famJog *two;
+    struct famJog *three;
+    struct famJog *four;
+    struct famJog *five;
+    struct famJog *six;
+    struct famJog *seven;
+    struct famJog *eight;
+    struct famJog *nine;
+    struct famJog *ten;
+    struct famJog *eleven;
+    struct famJog *twelve;
+    struct famJog *thirt;
+    struct famJog *fourt;
+    struct famJog *fift;
+    struct famJog *sixt;
+    struct famJog *sevent;
+    struct famJog *eightt;
+    struct famJog *ninet;
+    struct famJog *twenty;
+    struct famJog *tone;
+    struct famJog *ttwo;
+    struct famJog *tthree;
+    struct famJog *tfour;
+} *PARENTE;
+
+VALOR opp (VALOR peca);
+ESTADO jogadaFilho (ESTADO e, PARENTE x);
+
+int aux_minMAX (ESTADO e, PARENTE x, int valor);
+int aux_MINmax (ESTADO e, PARENTE x, int valor);
+
+int minmax(ESTADO e, int nivel, PARENTE x);
+
+PARENTE jogTree(PARENTE p, int altura);
+PARENTE criaRaiz(ESTADO e, PONTO posicao, int altura);
+PARENTE nivelTree(PARENTE p, int altura);
+
+#endif //PROJ_BOT_H
